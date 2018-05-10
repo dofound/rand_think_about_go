d82f785f286342f759fc62f435c24cea17028c1a
xiaojianhe <xiaojianhe@didichuxing.com>
2018-05-10 19:49:26 +0800

    up mess

--- string/os.go
+++ string/os.go
@@ -5,14 +5,15 @@ import (
 	//"strings"
 	"strconv"
 )
+
 func main() {
 	//(1)
 	var s string
-	s = "2 hello wold"
+	s = "hello wold!"
 	fmt.Println(s)
 
 	//(2)
-	d:="hello world 2"
+	d:="hello world,hi!"
 	fmt.Printf("%v\n",d)
 
 	//(3)
@@ -27,8 +28,10 @@ func main() {
 	if err !=nil {
 		fmt.Println(err)
 	}
+	gggg,_ := strconv.ParseInt(gg,10,64)
+	m := int64(dd) + gggg
 	mm := dd+ggg
-	fmt.Printf("%v\n",mm)
+	fmt.Printf("%v,%v\n",m,mm)
 
 
 }
