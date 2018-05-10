cd72920b55f8030e58b30d6f57f6997183a46ae1
xiaojianhe <xiaojianhe@didichuxing.com>
2018-05-10 19:23:15 +0800

    up

--- string/os.go
+++ string/os.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	//"strings"
 )
 func main() {
-	var s,sep string
-	for i:=1;i<len(os.Args);i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
+	var s string
+
+	s = "2 hello wold"
 	fmt.Println(s)
 
+	d:="hello world 2"
+
+	fmt.Printf("%v\n",d)
+
+	p := s+" "+d
+	fmt.Printf("%v\n",p)
 }
