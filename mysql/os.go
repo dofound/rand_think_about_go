c9b476d2ac2ac7eedf16d9167d23f3256bc0b9ec
xiaojianhe <xiaojianhe@didichuxing.com>
2018-05-15 15:37:47 +0800

    up mysql

--- mysql/os.go
+++ mysql/os.go
@@ -1,15 +1,47 @@
 package main
 
 import (
-	"os"
+	"database/sql"
 	"fmt"
+	"github.com/BurntSushi/toml"
+	_ "github.com/go-sql-driver/mysql"  //must import
 )
-func main() {
-	var s,sep string
-	for i:=1;i<len(os.Args);i++ {
-		s += sep + os.Args[i]
-		sep = " "
-	}
-	fmt.Println(s)
 
+func main() {
+
+	//Get the my.conf information
+	type dbConf struct {
+		Addr       string `toml:"addr"`
+		DriverName string `toml:"driver_name"`
+	}
+	type myConf struct {
+		Dbconf dbConf `toml:"database"`
+	}
+	var myconf myConf
+	_, err := toml.DecodeFile("my.conf", &myconf)
+	if err != nil {
+		fmt.Printf("decode err:%v", err)
+	}
+	fmt.Printf("conf :%v", myconf)
+
+	//connect db
+	db, err := sql.Open(myconf.Dbconf.DriverName, myconf.Dbconf.Addr)
+	if err != nil {
+		fmt.Printf("error:%v", err)
+	}
+	defer db.Close()
+	fmt.Printf("db:%v", db)
+
+	//get db info
+	sqlQeury := "select * from my limit 1"
+	row := db.QueryRow(sqlQeury)
+	var (
+		id, age int64
+		name    string
+	)
+	err = row.Scan(&id, &age, &name)
+	if err != nil {
+		fmt.Printf("scan:%v", err)
+	}
+	fmt.Printf("data:%d,%d,%s", id, age, name)
 }
